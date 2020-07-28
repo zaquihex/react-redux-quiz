301ac7537ad2d1363d58a54e562017ef353f1cc9
zaquiel.rodriguez.arce <zaquihex@gmail.com>
2020-07-28 23:20:08 +0200

    first commit

---
+++ src/constants/translations_en.js
@@ -0,0 +1,68 @@
+export const questionsReact = {
+    typeQuestion: 'checkButtons',
+    questions: [
+        {
+            question1: '¿Which hook is used to provide an state to the application ?',
+            options: [
+                {
+                    label: 'useState',
+                    value: { value: 'useState', answer: false }
+                },
+                {
+                    label: 'useEffect',
+                    value: { value: 'useEffect', answer: false }
+                },
+                {
+                    label: 'useContext',
+                    value: { value: 'useContext', answer: true }
+                },
+                {
+                    label: 'useReducer',
+                    value: { value: 'useReducer', answer: true }
+                },
+            ]
+        },
+        {
+            question2: '¿Which hook can replace componentdidupdate?',
+            options: [
+                {
+                    label: 'useEffect',
+                    value: { value: 'useEffect', answer: true }
+                },
+                {
+                    label: 'useCallback',
+                    value: { value: 'useCallback', answer: false }
+                },
+                {
+                    label: 'useMemo',
+                    value: { value: 'useMemo', answer: false }
+                },
+                {
+                    label: 'useImperativeHandle',
+                    value: { value: 'useImperativeHandle', answer: false }
+                },
+            ]
+        },
+        {
+            question2: '¿Which hook can replace componentdidupdate?',
+            options: [
+                {
+                    label: 'useEffect',
+                    value: { value: 'useEffect', answer: true }
+                },
+                {
+                    label: 'useCallback',
+                    value: { value: 'useCallback', answer: false }
+                },
+                {
+                    label: 'useMemo',
+                    value: { value: 'useMemo', answer: false }
+                },
+                {
+                    label: 'useImperativeHandle',
+                    value: { value: 'useImperativeHandle', answer: false }
+                },
+            ]
+        }
+    ]
+}
